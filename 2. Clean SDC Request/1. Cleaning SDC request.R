2669cdda1d095dbb4cea462324d65cc40c1cb38f
Ekaterina Volkova <orhahog@gmail.com>
2017-08-09 13:12:42 -0400

    2017-08-01

--- 2. Clean SDC Request/1. Cleaning SDC request.R
+++ 2. Clean SDC Request/1. Cleaning SDC request.R
@@ -35,8 +35,11 @@ colnames(ipo) <- c("Filing_date", "Issue_date", "Issuer", "State", "Nation","IPO
                   "Mgr_codes","Tech_ind", "Low_Price", "High_Price", "Low_Price_History", "High_Price_History")
 ipo <- as.data.table(ipo)
 
+### The exact number of downloaded observation depends on SDC version
+### In our sample SDC report consisnt of 43817 obs
+
 ### keep only IPOs
-### 40,695 obs -> 14,589
+### 43.817 obs -> 16,454
 ipo <- ipo[IPO_Flag != "No" & Original_IPO_Flag != "No"]
 ### format dates
 ipo[, `:=`(Filing_date = mdy(Filing_date), Issue_date = mdy(Issue_date))]
@@ -46,14 +49,14 @@ ipo[, Offer_Price := gsub(",","", Offer_Price)]
 ipo[, Offer_Price := as.numeric(as.character(Offer_Price))]
 
 ### types of securities we will keep in the data (common/ordinary shares)
-### 14,589 obs before ---> 13,956 obs after
+### 15,454 obs before ---> 15,107 obs after
 ### Comment: sometimes Ritter codes "MLP-Common Shs" as units =1
 ex_types <- c("Units", "Ltd Prtnr Int", "MLP-Common Shs", "Shs Benficl Int",
              "Ltd Liab Int", "Stock Unit", "Trust Units", "Beneficial Ints")
 ipo <- ipo[!Type %in% ex_types,]
 
 ### drop REIT, Units, ADR, penny stocks and CEF
-### 13,956 obs before ---> 10,645 obs after
+### 15,107 obs before ---> 11,103 obs after
 logic <- ipo$REIT == "" & (ipo$Unit == "No" | ipo$Unit == "") &  ipo$Depositary == "No" & 
   !is.na(ipo$Offer_Price) & ipo$Offer_Price >= 5 & 
   ipo$CEF == "No" 
@@ -91,20 +94,21 @@ ipo[(dif >= -1 & dif <= 7) & (Permno == - 999) & !is.na(dif), Permno := Permno_n
 ipo[, dif := n(First_CRSP_date_ncusip6 - Issue_date)]
 ipo[ (dif >= -1 & dif <= 7) & (Permno == - 999) & !is.na(dif), Permno := Permno_ncusip6]
 
-### Dropping non-match companies
-### 10,654 obs ---> 9,822 obs
+### Dropping non-match companies and duplicated
+### (Duplicates usually happen when Units and Common Stocks are offered at the same time)
+### 11,103 obs ---> 8,995 obs
 ipo <- ipo[!(Permno == -999)]
+ipo <- ipo[!duplicated(ipo$Permno)]
 
+### matching CRSP infor
 m <- match(ipo$Permno, crsp$PERMNO)
 ipo[, `:=` (First_CRSP_date = ymd(crsp$date[m]), Close_price1 = abs(crsp$PRC[m]), Close_price2 = abs(crsp$PRC[m + 1]))]
+
+### removing extra variables
 ipo[,`:=`(First_CRSP_date_ncusip = NULL,First_CRSP_date_ncusip6 = NULL, Permno_ncusip = NULL, Permno_ncusip6 = NULL, dif = NULL)]
 ipo[, `:=`(REIT = NULL, Unit = NULL, Depositary = NULL, CEF = NULL, CUSIP = NULL, CUSIP9 = NULL)]
 ipo[, `:=`(IPO_Flag = NULL, Original_IPO_Flag = NULL)]
 
-### Dropping duplicated observations:
-### 9,822 obs ---> 8,821 obs 
-### (This usually happens when Units and Common Stocks are offered at the same time)
-ipo <- ipo[!duplicated(ipo$Permno)]
 
 ### Dropping wrong share clases and shares traded on other exchanges
 ### Loading CRSP Stock Header Information file
